67e54a07ec45828248ba43172bc991f0ffea1b20
student-proger <nightflash@live.ru>
2020-10-23 23:26:56 +0700

    Added installer script

--- _build.cmd
+++ _build.cmd
@@ -4,3 +4,5 @@ md dist\main\ArduinoFW\BellManagerController
 copy ArduinoFW\BellManagerController\*.* dist\main\ArduinoFW\BellManagerController\
 copy images\*.* dist\main\images\
 copy license.txt dist\main\
+rename dist\main\main.exe bellmanager.exe
+rename dist\main\main.exe.manifest bellmanager.exe.manifest
